85754a79ee740b792c3fc5a2ffc3ef50fa4bb6a0
Supratimdas20 <supratimdaswork@gmail.com>
2024-09-27 20:11:08 +0530

    feat:Schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from"express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app =express();
 
@@ -21,9 +24,10 @@ const corsOptions={
     credentials:true
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
 
